2e3c8879b223ec83c4eb8e5e5411a9fccaa95dae
Bsoyoung <qksthdhd94@gmail.com>
2020-08-22 14:46:35 +0900

    login

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -14,16 +14,15 @@ class Kiwoom(QAxWidget):
         #####초기 셋팅 함수들 바로 실행
         self.get_ocx_instance()#OCX 방식을 파이썬에 사용할 수 있게 변환해 주는 함수
         self.event_slots()  # 키움과 연결하기 위한 시그널/ 슬롯 모음
-        #self.signal_login_commConnect()  # 로그인 요청 함수 포함
-        #self.get_account_info() #계좌번호 가져오기
+        self.signal_login_commConnect()  # 로그인 요청 함수 포함
+        self.get_account_info() #계좌번호 가져오기
         ######################################
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1") # 레지스트리에 젖아된 api 모듈 불러오기
 
     def event_slots(self):
-        print("blah")
-        #self.OnEventConnect.connect(self.login_slot) #로그인 관련 이벤트
+        self.OnEventConnect.connect(self.login_slot) #로그인 관련 이벤트
 
     def signal_login_commConnect(self):
         self.dynamicCall("CommConnect()") #로그인 요청 시그널
